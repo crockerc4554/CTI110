f04f718ccbe5550aa9467da291ee43e7a24789e9
crockerc4554 <31869460+crockerc4554@users.noreply.github.com>
2017-09-11 16:52:12 -0700

    Add files via upload

---
+++ M2T1_SalesPrediction_crocker.py
@@ -0,0 +1,13 @@
+## CTI-110 
+# M2T1 - Sales Prediction 
+# Crocker Clintonia a
+# 11sept2017
+
+# Get the projected total sales.
+total_sales= float(input('Enter the projected sales: '))
+
+# calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+ # display the profit.
+print('The profit is $', format(profit, ',.2f'))
