0839735977377e28581c1ceef97dfee958d13d22
crockerc4554 <31869460+crockerc4554@users.noreply.github.com>
2017-09-11 17:28:03 -0700

    Add files via upload
    first submission

---
+++ M2HW1_DistanceTraveled_CROCKER.py
@@ -0,0 +1,46 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# CROCKER
+# 11 SEPT 2017
+#
+
+#get the time of car.
+time= float(input('enter the time of car as 6 to find the distance traveled at 70 MPH for 6 hours: '))
+
+#get the speed of travel.
+speed= 70
+
+#Get the total distance of car.
+distance = time * .70
+
+
+#display total distance.
+print('The distance after 6 hours is', format(distance, ',.2f'))
+
+#get the time of car.
+time= float(input('enter the time of car as 15 to find the distance traveled at 70 MPH for 15 hours: '))
+
+#get the speed of travel.
+speed= 70
+
+#Get the total distance of car.
+distance = time * .70
+
+
+#display total distance.
+print('The distance after 15 hours is', format(distance, ',.2f'))
+
+#get the time of car.
+time= float(input('enter the time of car as 10 to find the distance traveled at 70 MPH for 10 hours: '))
+
+#get the speed of travel.
+speed= 70
+
+#Get the total distance of car.
+distance = time * .70
+
+
+#display total distance.
+print('The distance after 10 hours is', format(distance, ',.2f'))
+
+
